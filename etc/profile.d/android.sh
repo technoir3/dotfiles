2d7f18990200087a5d15a6dcc61fc30796d91cef
sergey <sergey.chelombitko@gmail.com>
2015-07-11 19:39:51 +0300

    Update NDK path

--- etc/profile.d/android.sh
+++ etc/profile.d/android.sh
@@ -1,3 +1,3 @@
 export ANDROID_HOME=/opt/android-sdk-linux
-export ANDROID_NDK_HOME=/opt/android-ndk-r10d
+export ANDROID_NDK_HOME=/opt/android-ndk-r10e
 PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME
